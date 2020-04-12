036aa709eae11e55d875061136390468896bd100
gusmv <gustavo-mv@outlook.com>
2020-04-12 17:19:23 -0300

    Front-end da aplicação

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import Header from './components/Header';
+import Signin from './pages/signin';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function ReactRoutes() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Switch>
+                <Route path="/" exact={true} component={Signin} />
+            </Switch>
+        </ BrowserRouter>
+    )
+}
+
+ReactDOM.render(<ReactRoutes />, document.getElementById('root'));
