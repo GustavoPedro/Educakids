1a0e3670e964fff4b6b3239797004476ebb228f9
gusmv <gustavo-mv@outlook.com>
2020-04-12 20:01:20 -0300

    Front-end

--- src/services/auth.js
+++ src/services/auth.js
@@ -5,4 +5,4 @@ export const getToken = () => localStorage.getItem(TOKEN_KEY);
 export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY) !== null;
 export const login = token => {localStorage.setItem(TOKEN_KEY, token);};
 export const logout = () => {localStorage.removeItem(TOKEN_KEY);};
-export const getRole = () => jwt_decode(getToken()).role;
+export const getRole = () => getToken() ? jwt_decode(getToken()).role : "";
