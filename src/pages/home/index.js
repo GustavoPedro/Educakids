92a38c03a4eaac111c31e1a8c8dc326013805cfd
gusmv <gustavo-mv@outlook.com>
2020-04-13 18:15:57 -0300

    CRUD USUARIO

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,39 +1,111 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../../services/api';
 
 import './styles.css';
 
 export default function Home() {
+    const [cpf, setCpf] = useState('');
+    const [senha, setSenha] = useState('');
+    const [senhaStatus, setSenhaStatus] = useState('');
+
+    const [NomeSobrenome, setNome] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefone, setTelefone] = useState('');
+
+    if (!NomeSobrenome) {
+      api.get('/api/Usuario')
+      .then((resp => {
+        setCpf(resp.data.Cpf)
+        setNome(resp.data.NomeSobrenome);
+        setEmail(resp.data.Email);
+        setTelefone(resp.data.Telefone);
+      }))
+      .catch((error => {
+        console.log(error)
+      }))
+    }
+
+    async function changePassword() {
+      if (senha !== "") {
+        setSenha('');
+        await api.put('/api/Usuarios/Senha', {email, senha})
+        .then((e => setSenhaStatus('success')))
+        .catch((e => setSenhaStatus('fail')))
+      } else {
+        setSenhaStatus('fail');
+      }
+    }
+
+    async function updateUserInfo() {
+        await api.put('/api/Usuario', {
+          cpf, email, telefone, NomeSobrenome})
+        .then((resp => console.log(resp.data)))
+    
+    }
+
+    function formatTel(value) {
+      return value
+        .replace(/\D/g, '') 
+        .replace(/^(\d{2})(\d)/g, '($1) $2') 
+        .replace(/(\d)(\d{4})$/,'$1-$2')
+    }
+
     return (
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-4">
-            <h1 className="my-4">Bem vindo, <small> Gustavo!</small></h1>
-
+          <div className="col-md-7">
+            <h1 className="my-4">Bem vindo, <small> {NomeSobrenome}!</small></h1>
             <div className="card mb-4">
               <div className="card-body">
-                <h2 className="card-title">Post Title</h2>
-                <p className="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam. Dicta expedita corporis animi vero voluptate voluptatibus possimus, veniam magni quis!</p>
-                <a href="#" className="btn btn-primary">Read More &rarr;</a>
+                <h2 className="card-title">algo</h2>
+                <p className="card-text">cala boca</p>
               </div>
             </div>
           </div>
 
-          <div className="col-md-8">
+          <div className="col-md-5">
             <div className="card my-4">
               <h5 className="card-header">Editar dados</h5>
               <div className="card-body">
-                <input type="text" className="form-control mb-2" value={'Gustavo Miranda'} />
-                <input type="text" className="form-control mb-2" value={'gustavo-mv@outlook.com'} />
-                <input type="text" className="form-control mb-2" value={'(31) 997872268'} />
-                <button type="button" className="btn btn-primary w-100">Salvar</button>
+                <input type="text" 
+                  onChange={e => setNome(e.target.value)}
+                  className="form-control mb-2" value={NomeSobrenome} />
+                <input 
+                  onChange={e => setEmail(e.target.value)}
+                  type="text" className="form-control mb-2" value={email} 
+                  disabled />
+                <input type="text" 
+                  maxLength="15"
+                  onChange={e => setTelefone(formatTel(e.target.value))}
+                  className="form-control mb-2" value={telefone} />
+                <button 
+                  onClick={e => updateUserInfo()}
+                  type="button" className="btn btn-primary w-100">Salvar</button>
 
               </div>
             </div>
               <div className="card my-4">
                 <h5 className="card-header">Trocar senha</h5>
                 <div className="card-body">
-                  <input type="password" className="form-control mb-2" placeholder='Nova senha' />
-                  <button type="button" className="btn btn-primary w-100">Salvar</button>
+                  {senhaStatus === 'success' && 
+                      <div className="alert alert-success" role="alert">
+                          Senha alterada com sucesso.
+                      </div>
+                  }
+                  {senhaStatus === 'fail' && 
+                      <div className="alert alert-danger" role="alert">
+                          Por favor, escolha outra senha.
+                      </div>
+                  }
+                  <input 
+                    type="password" 
+                    className="form-control mb-2" 
+                    placeholder='Nova senha'
+                    value={senha}
+                    onChange={e => setSenha(e.target.value)} />
+                  <button type="button" 
+                    onClick={e => changePassword()}
+                    className="btn btn-primary w-100">Salvar</button>
               </div>
             </div>
           </div>
